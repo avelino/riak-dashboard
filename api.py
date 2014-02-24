c48ee7fa45a2cee71043e6e8653e42ebf9aa7c72
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-24 17:24:26 -0300

    change urllib2 to requests

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from urllib2 import urlopen
+import requests
 
 from bottle import route, run, response, request
 
@@ -17,14 +17,13 @@ def index(path):
     response.set_header('charset', 'UTF-8')
 
     j = "&".join("%s=%s" % tup for tup in request.GET.items())
-    conn = urlopen('http://127.0.0.1:8098/{}?{}'.format(path, j))
+    re = requests.get('http://127.0.0.1:8098/{}?{}'.format(path, j))
 
     if (request.query.callback):
         response.content_type = "application/javascript"
-        return jsonp(request, conn.read())
-    else:
-        response.content_type = 'application/json'
-        return conn.read()
+        return jsonp(request, re.text)
+    response.content_type = 'application/json'
+    return re.text
 
 
 run(host='127.0.0.1', port=8889)
