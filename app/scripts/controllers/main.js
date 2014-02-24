93a202071c21805a76fb902d602f54b59a62338a
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-24 17:05:54 -0300

    clean controller MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,7 @@
 'use strict';
 
 angular.module('riakDashboardApp')
-  .factory('Bucket', ['$resource', function($resource) {
-    return $resource('http://127.0.0.1:8889/buckets', {}, {
-      get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}}
-    });
-  }])
-
-  .controller('MainCtrl', ['$scope', '$http', 'Bucket', function ($scope, $http, Bucket) {
-
-    var lerolero = Bucket.get();
-    console.log(lerolero)
-
+  .controller('MainCtrl', ['$scope', 'Bucket', function ($scope, Bucket) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
