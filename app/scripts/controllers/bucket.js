1e4d0d08f03d405587af6aed798f063a3c54074a
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-24 18:34:42 -0300

    Add method reset bucket

--- app/scripts/controllers/bucket.js
+++ app/scripts/controllers/bucket.js
@@ -1,15 +1,28 @@
 'use strict';
 
-angular.module('riakDashboardApp')
-  .factory('Bucket', ['$resource', function($resource) {
-    return $resource('http://127.0.0.1:8889/buckets', {}, {
-      jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}}
-    });
-  }])
+var JSONP_RESOURCE = function($resource, URL){
+  return $resource(URL, {}, {
+    jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}},
+    delete: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}}
+  });
+}
 
-  .controller('BucketCtrl', ['$scope', 'Bucket', function ($scope, Bucket) {
-    Bucket.jsonp(function(b){
+angular.module('riakDashboardApp')
+  .controller('BucketCtrl', ['$scope', '$http', '$resource', function ($scope, $http, $resource) {
+    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets')
+    .jsonp(function(b){
       $scope.buckets = b.buckets;
     });
+
+    $scope.bucket_reset = function (bucket) {
+      JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ bucket +'/props')
+      .delete(function(b){
+        console.log(b)
+      });
+    };
+
+    $scope.bucket_delete = function (bucket) {
+    };
+
     $scope.menu_bucket = true;
   }]);
