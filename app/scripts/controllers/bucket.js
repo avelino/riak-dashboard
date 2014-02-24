dbe9766355b17503eb3890ccb976888547027e68
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-24 17:04:07 -0300

    create BucketCtrl, get rest api buckets

---
+++ app/scripts/controllers/bucket.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('riakDashboardApp')
+  .factory('Bucket', ['$resource', function($resource) {
+    return $resource('http://127.0.0.1:8889/buckets', {}, {
+      jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}}
+    });
+  }])
+
+  .controller('BucketCtrl', ['$scope', 'Bucket', function ($scope, Bucket) {
+    Bucket.jsonp(function(b){
+      $scope.buckets = b.buckets;
+    });
+  }]);
