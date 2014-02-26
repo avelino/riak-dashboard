aec6f4c264e3f4c8fa9c323e3f8bad2ae5d95885
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-26 14:33:54 -0300

    create new ctrl bucket list

--- app/scripts/controllers/bucket.js
+++ app/scripts/controllers/bucket.js
@@ -2,26 +2,40 @@
 
 var JSONP_RESOURCE = function($resource, URL){
   return $resource(URL, {}, {
-    jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}},
-    delete: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', buckets: true}}
+    jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'get'}},
+    delete: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'delete'}}
   });
 }
 
 angular.module('riakDashboardApp')
   .controller('BucketCtrl', ['$scope', '$http', '$resource', function ($scope, $http, $resource) {
-    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets')
+    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets?buckets=true')
     .jsonp(function(b){
       $scope.buckets = b.buckets;
     });
 
     $scope.bucket_reset = function (bucket) {
       JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ bucket +'/props')
-      .delete(function(b){
-        console.log(b)
-      });
+      .delete();
     };
 
     $scope.bucket_delete = function (bucket) {
+    };
+
+    $scope.menu_bucket = true;
+  }])
+
+  .controller('BucketListCtrl', ['$scope', '$resource', '$routeParams', '$timeout', function ($scope, $resource, $routeParams, $timeout) {
+    var slug = $routeParams.slug;
+    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ slug +'/keys?keys=true')
+      .jsonp(function(b){
+        $scope.keys = b.keys;
+    });
+
+    $scope.bucket_delete = function (key) {
+      JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ slug +'/keys/'+ key)
+      .delete();
+      $scope.keys.splice($scope.keys.indexOf(key), 1)
     };
 
     $scope.menu_bucket = true;
