9ab200c2724dac544e369f5f0eab14ea459e30b0
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-26 14:50:41 -0300

    Create factory $jsonp, rewrite $resource and render jsonp http method

--- app/scripts/controllers/bucket.js
+++ app/scripts/controllers/bucket.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var JSONP_RESOURCE = function($resource, URL){
-  return $resource(URL, {}, {
-    jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'get'}},
-    delete: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'delete'}}
-  });
-}
-
 angular.module('riakDashboardApp')
-  .controller('BucketCtrl', ['$scope', '$http', '$resource', function ($scope, $http, $resource) {
-    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets?buckets=true')
-    .jsonp(function(b){
+  .factory('$jsonp', ['$resource', function($resource){
+    return function(URL){
+      return $resource(URL, {}, {
+        jsonp: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'get'}},
+        delete: {method: 'JSONP', params: {callback: 'JSON_CALLBACK', _method: 'delete'}}
+      });
+    };
+  }])
+
+  .controller('BucketCtrl', ['$scope', '$http', '$jsonp', function ($scope, $http, $jsonp) {
+    $jsonp('http://127.0.0.1:8889/buckets?buckets=true').jsonp(function(b){
       $scope.buckets = b.buckets;
     });
 
     $scope.bucket_reset = function (bucket) {
-      JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ bucket +'/props')
-      .delete();
+      $jsonp('http://127.0.0.1:8889/buckets/'+ bucket +'/props').delete();
     };
 
     $scope.bucket_delete = function (bucket) {
@@ -25,16 +25,16 @@ angular.module('riakDashboardApp')
     $scope.menu_bucket = true;
   }])
 
-  .controller('BucketListCtrl', ['$scope', '$resource', '$routeParams', '$timeout', function ($scope, $resource, $routeParams, $timeout) {
+  .controller('BucketListCtrl', ['$scope', '$jsonp', '$routeParams', '$jsonp', function ($scope, $jsonp, $routeParams) {
     var slug = $routeParams.slug;
-    JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ slug +'/keys?keys=true')
+
+    $jsonp('http://127.0.0.1:8889/buckets/'+ slug +'/keys?keys=true')
       .jsonp(function(b){
         $scope.keys = b.keys;
-    });
+      });
 
     $scope.bucket_delete = function (key) {
-      JSONP_RESOURCE($resource, 'http://127.0.0.1:8889/buckets/'+ slug +'/keys/'+ key)
-      .delete();
+      $jsonp('http://127.0.0.1:8889/buckets/'+ slug +'/keys/'+ key).delete();
       $scope.keys.splice($scope.keys.indexOf(key), 1)
     };
 
