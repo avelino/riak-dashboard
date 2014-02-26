31b0e470e466c9576aa04ad0b986d0251dff4366
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-26 15:54:11 -0300

    get riak api stats

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,13 @@
 'use strict';
 
 angular.module('riakDashboardApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  .controller('MainCtrl', ['$scope', '$jsonp', function ($scope, $jsonp) {
+
+    $jsonp('http://127.0.0.1:8889/stats')
+      .jsonp(function(s){
+        $scope.stats = s;
+      });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
